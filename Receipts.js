432cdebde92d9e6400d80c04070f106e94c1bc11
Wiktor M <v460gle@wp.pl>
2019-02-20 16:53:57 +0100

    Zmiana sprawdzania poprawności paragonu - dodanie weryfikacji kwoty

--- Receipts.js
+++ Receipts.js
@@ -1,6 +1,6 @@
-let RegisteredReceipts = [ "ABC12345678901233", "ABC0987654321234", "ABC11223344556677", "ABC09876543210987" ]; 
+let RegisteredReceipts = [ "ABC12345678901233", "ABC0987654321234", "ABC11223344556677", "ABC09876543210987" ];
 
-function ShowReceiptInput() 
+function ShowReceiptInput()
 {
     ReceiptDiv.style.display = "block";
     const ReceiptNumber = "<input id = \"receiptnumber\" type = \"text\" value=\"Nr paragonu\">";
@@ -15,7 +15,7 @@ function AddReceipt() {
     const ReceiptNumberControl = document.getElementById("receiptnumber");
     const ReceiptTotalSumControl = document.getElementById("receipttotalamount");
     const ReceiptTotalSum = parseInt(ReceiptTotalSumControl.value);
-    const ProperReceipt = ValidateReceipt(ReceiptNumberControl.value);
+    const ProperReceipt = ValidateReceipt(ReceiptNumberControl.value, ReceiptTotalSum);
     if (ProperReceipt == true) {
         if (ReceiptTotalSum <= 10000 && ReceiptTotalSum > 0) {
             ZalogowanyUżytkownik.punkty += ReceiptTotalSum;
@@ -23,6 +23,7 @@ function AddReceipt() {
             ReceiptNumberControl.value = "";
             ReceiptTotalSumControl.value = 0;
             ReceiptDiv.style.display = "none";
+            window.alert("Do Twojego konta dodano " +  ReceiptTotalSum + " punktów");
         }
         else
             window.alert("Nieprawidłowa kwota");
@@ -30,19 +31,32 @@ function AddReceipt() {
     ShowAccount();
 }
 
-function ValidateReceipt(receiptnumber) {
+function ValidateReceipt(receiptnumber, receiptvalue) {
     var validated = false;
-    if (receiptnumber.startsWith("ABC") && receiptnumber.length == 17) {
+    if (receiptnumber.startsWith("ABC") && receiptnumber.length == 17)
+    {
         validated = true;
+        var sumEncodedInReceiptNumber = parseInt(receiptnumber.substring(17,13));
+
         RegisteredReceipts.forEach(element => {
             if (element == receiptnumber) {
                 validated = false;
                 window.alert("Ten paragon był już rejestrowany");
             }
         });
+
+        if(validated)
+        {
+            if(sumEncodedInReceiptNumber != receiptvalue)
+            {
+                validated = false;
+                window.alert("Niepoprawna kwota paragonu " + receiptnumber);
+            }
+        }
     }
     else {
         window.alert("Niepoprawny numer paragonu");
     }
     return validated;
 }
+
